5a84d47c67d50eae7bd1c76747b16f5fbf577444
alexander <alexander.s.gavrilov@yandex.com>
2018-06-16 21:08:40 +0400

    Expected conditions + Singleton implementation for WebDriverManager

--- src/main/java/utils/WebDriverManager.java
+++ src/main/java/utils/WebDriverManager.java
@@ -3,23 +3,62 @@ package utils;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class WebDriverManager {
 
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
+        String driverFromProperties =
+                PropertyReader.getPropertyFromFile("properties/config.properties", "browser");
         if (driver == null) {
-            //ChromeOptions options = new ChromeOptions();
-            //options.addArguments("--headless");
-            driver = new ChromeDriver();
+            switch (driverFromProperties) {
 
+                case "mozilla":
+                    createFirefoxDriver();
+                    break;
+                case "ie":
+                    createIEDriver();
+                    break;
+                case "chrome":
+                    createChromeDriver(false);
+                    break;
+                case "chrome-headless":
+                    createChromeDriver(true);
+                    break;
+                default:
+                    createChromeDriver(false);
+                    break;
+            }
         }
         return driver;
+    }
 
+    private static WebDriver createChromeDriver(Boolean headless) {
+        if (headless) {
+            io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else {
+            io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        return driver;
+    }
+
+
+    private static WebDriver createFirefoxDriver() {
+        io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    private static WebDriver createIEDriver() {
+        io.github.bonigarcia.wdm.WebDriverManager.iedriver().setup();
+        driver = new InternetExplorerDriver();
+        return driver;
     }
 }
-
--- src/test/java/tests/com/automationpractice/automationPracticeExpectedConditionsTest.java
+++ src/test/java/tests/com/automationpractice/automationPracticeExpectedConditionsTest.java
@@ -4,20 +4,20 @@ import com.automationpractice.indexPage;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class autoPracticeIETest extends ieBaseTest {
+public class automationPracticeExpectedConditionsTest extends ECBaseTest {
 
     private static final String URL = "http://automationpractice.com/index.php";
 
+
     @Test
     public void autoPracticeChromeTest() {
+
         driver.get(URL);
         indexPage indexPage = new indexPage(driver);
         indexPage.mouseOverWomen();
         indexPage.tshirtMenuItemToClick();
-
         Assert.assertTrue(indexPage.getResultsContainer().isDisplayed());
         Assert.assertTrue(indexPage.getResultsContainerItem().isDisplayed());
         Assert.assertTrue(indexPage.validateResults() != 0);
-
     }
 }
